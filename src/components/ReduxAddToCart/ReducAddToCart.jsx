631a929e4bcf26c3ea918c8e068bb0cf3d8da94c
Suswetha <suswetha.23bcs10166@ms.sst.com>
2024-06-24 11:00:51 +0530

    redux functions

---
+++ src/components/ReduxAddToCart/ReducAddToCart.jsx
@@ -0,0 +1,37 @@
+import { useSelector,useDispatch } from "react-redux";
+
+function AddToCart({ product }) {
+  let dispatch =useDispatch();
+  function increase() {
+      dispatch({type:"ADD_TO_CART",payload:product});
+  }
+  function decrease() {
+    dispatch({type:"REMOVE_FROM_CART",payload:product}); 
+  }
+
+  let cart =useSelector((state)=>{
+    return state.items;
+  })
+
+  const quantity = cart[product.id] ? cart[product.id].quantity : 0;
+  if (quantity === 0) {
+      return (
+          <div>
+              <button onClick={increase}>AddToCart</button>
+          </div>
+   )  
+  } else {
+      return ( 
+          <div>
+              <button onClick={decrease}>-</button>
+              <span>{quantity}</span>
+              <button onClick={increase}>+</button>
+
+          </div>
+      )
+  }
+
+}
+
+export default AddToCart;
+
